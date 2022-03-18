3abac4bd1448fa37314a72eed5039616cc8e81e0
onkar-shaligram <onkarshaligram01@gmail.com>
2022-03-19 00:35:09 +0530

    First commit🚀

---
+++ lib/main.dart
@@ -0,0 +1,20 @@
+import 'package:agora_video_call/pages/HomePage.dart';
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Group Video Calling',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        accentColor: Colors.blueAccent,
+      ),
+      home: MyHomePage(),
+    );
+  }
+}
